87e5ae24574fd7ad29b4480fbd4282289fe0c1e3
Matthew Elliott <melliot1@ucsc.edu>
2021-02-09 13:21:39 -0800

    Update david.sh

--- Code/Users/david.sh
+++ Code/Users/david.sh
@@ -2,3 +2,8 @@
 # alias commands David likes
 echo 'alias aws3="aws --endpoint https://s3.nautilus.optiputer.net s3"' >> ~/.bashrc
 echo 'alias awsn="aws --endpoint https://s3.nautilus.optiputer.net"' >> ~/.bashrc
+
+
+# Packages for IoT Messaging in braingeneers
+pip install awsiot
+pip install awscrt
