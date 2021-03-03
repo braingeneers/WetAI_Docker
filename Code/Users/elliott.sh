481d5dd5e2a18750dc2f7eb7c4ad5c7e9546bd5c
Matthew Elliott <melliot1@ucsc.edu>
2021-03-03 12:47:43 -0800

    Update elliott.sh

--- Code/Users/elliott.sh
+++ Code/Users/elliott.sh
@@ -7,10 +7,14 @@
 
 # Generally Useful
 pip install timeago 
+apt install -y  zip unzip # allows zipping files
 #pip install --upgrade google-cloud-datastore #pip install --upgrade firebase-admin
 #pip install tensorflow==2.0.0-alpha0  #pip install keras
 
-### Agors App
+# Welcome Notebooke
+pip install --upgrade nbstripout # used to clean up notebooks before committing them
+
+### Agora App
 pip3 install PyGithub
 
 
