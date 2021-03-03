5864cf101f1ad0d4a737230093aada3f42373648
Matthew Elliott <melliot1@ucsc.edu>
2021-03-03 12:43:15 -0800

    Update elliott.sh

--- Code/Users/elliott.sh
+++ Code/Users/elliott.sh
@@ -7,7 +7,8 @@
 
 # Generally Useful
 pip install timeago 
-
+#pip install --upgrade google-cloud-datastore #pip install --upgrade firebase-admin
+#pip install tensorflow==2.0.0-alpha0  #pip install keras
 
 ### Agors App
 pip3 install PyGithub
