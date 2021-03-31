855f7e23052bc26db85bb91f8467069e9c9cf1f0
Matthew Elliott <melliot1@ucsc.edu>
2021-03-31 16:33:59 -0700

    Update david.sh

--- Code/Users/david.sh
+++ Code/Users/david.sh
@@ -2,6 +2,7 @@
 # alias commands David likes
 #echo 'alias aws3="aws --endpoint https://s3.nautilus.optiputer.net s3"' >> ~/.bashrc
 #echo 'alias awsn="aws --endpoint https://s3.nautilus.optiputer.net"' >> ~/.bashrc
+# Set alias: https://stackoverflow.com/questions/36388465/how-to-set-bash-aliases-for-docker-containers-in-dockerfile
 echo -e '#!/bin/bash\naws --endpoint https://s3.nautilus.optiputer.net s3 "$@"' > /usr/bin/aws3 && chmod +x /usr/bin/aws3
 echo -e '#!/bin/bash\naws --endpoint https://s3.nautilus.optiputer.net "$@"' > /usr/bin/awsn && chmod +x /usr/bin/awsn
 
