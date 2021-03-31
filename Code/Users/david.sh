2fe45c7336034cf80925bc755e4debe0a9074376
Matthew Elliott <melliot1@ucsc.edu>
2021-03-31 14:39:20 -0700

    Update david.sh

--- Code/Users/david.sh
+++ Code/Users/david.sh
@@ -1,7 +1,9 @@
 
 # alias commands David likes
-echo 'alias aws3="aws --endpoint https://s3.nautilus.optiputer.net s3"' >> ~/.bashrc
-echo 'alias awsn="aws --endpoint https://s3.nautilus.optiputer.net"' >> ~/.bashrc
+#echo 'alias aws3="aws --endpoint https://s3.nautilus.optiputer.net s3"' >> ~/.bashrc
+#echo 'alias awsn="aws --endpoint https://s3.nautilus.optiputer.net"' >> ~/.bashrc
+echo -e '#!/bin/bash\naws --endpoint https://s3.nautilus.optiputer.net s3 "$@"' > /usr/bin/aws3 && chmod +x /usr/bin/aws3
+echo -e '#!/bin/bash\naws --endpoint https://s3.nautilus.optiputer.net "$@"' > /usr/bin/awsn && chmod +x /usr/bin/awsn
 
 # AWS Code
 apt-get install -y --no-install-recommends awscli # Install aws
