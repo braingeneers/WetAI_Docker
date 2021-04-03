75d4184baf3238725cb975eec10683fa99f5a2dd
Matthew Elliott <melliot1@ucsc.edu>
2021-04-03 14:22:54 -0700

    Update david.sh

--- Code/Users/david.sh
+++ Code/Users/david.sh
@@ -5,18 +5,16 @@
 #echo 'S3_ENDPOINT="s3.nautilus.optiputer.net"' >> ~/.profile
 #echo 'AWS_LOG_LEVEL=3' >> ~/.profile
 #echo 'TF_CPP_MIN_LOG_LEVEL=3' >> ~/.profile
-
-
 # alias commands David likes
 #echo 'alias aws3="aws --endpoint https://s3.nautilus.optiputer.net s3"' >> ~/.bashrc
 #echo 'alias awsn="aws --endpoint https://s3.nautilus.optiputer.net"' >> ~/.bashrc
-# Set alias: https://stackoverflow.com/questions/36388465/how-to-set-bash-aliases-for-docker-containers-in-dockerfile
-echo -e '#!/bin/bash\naws --endpoint https://s3.nautilus.optiputer.net s3 "$@"' > /usr/bin/aws3 && chmod +x /usr/bin/aws3
-echo -e '#!/bin/bash\naws --endpoint https://s3.nautilus.optiputer.net "$@"' > /usr/bin/awsn && chmod +x /usr/bin/awsn
 
 # AWS Code
 apt-get install -y --no-install-recommends awscli # Install aws
 cd ~ && mkdir .aws && touch ~/.aws/credentials  # Setup for AWS credentials file
+# Set alias: https://stackoverflow.com/questions/36388465/how-to-set-bash-aliases-for-docker-containers-in-dockerfile
+echo -e '#!/bin/bash\naws --endpoint https://s3.nautilus.optiputer.net s3 "$@"' > /usr/bin/aws3 && chmod +x /usr/bin/aws3
+echo -e '#!/bin/bash\naws --endpoint https://s3.nautilus.optiputer.net "$@"' > /usr/bin/awsn && chmod +x /usr/bin/awsn
 
 
 # Packages for IoT Messaging in braingeneers
