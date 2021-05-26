5b70b0e9f83de1e88aa468b2b5fd30184367924b
Matthew Elliott <melliot1@ucsc.edu>
2021-05-26 13:39:25 -0700

    Update nico.sh

--- Code/Users/nico.sh
+++ Code/Users/nico.sh
@@ -2,7 +2,10 @@
 ### Code for Spiksorter App
 
 # Install SpikeSort
-pip install spikeinterface ml_ms4alg boto3
+#!pip install spiketoolkit==0.7.2
+#!pip install spikewidgets==0.5.1
+pip install spikeinterface==0.11.0
+pip install ml_ms4alg boto3
 apt-get -y update
 apt-get -y install vim
 pip install --upgrade kachery
