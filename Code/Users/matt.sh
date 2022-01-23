9b3580958dbcbaff7697bbfa6aa0593bd19cf72b
Matthew Elliott <melliot1@ucsc.edu>
2022-01-22 22:32:07 -0800

    Update matt.sh

--- Code/Users/matt.sh
+++ Code/Users/matt.sh
@@ -1,6 +1,7 @@
 ### Code for Matt Keefe
 git clone https://github.com/hengenlab/musclebeachtools_hlab.git
 pip install musclebeachtools_hlab/
+rm -rf musclebeachtools_hlab
 
 git clone https://github.com/hengenlab/neuraltoolkit.git
 pip install neuraltoolkit/
