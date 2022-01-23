cab242486aa571551968b06ab1d0711fc05e6e40
Matthew Elliott <melliot1@ucsc.edu>
2022-01-22 21:58:50 -0800

    Update alex.sh

--- Code/Users/alex.sh
+++ Code/Users/alex.sh
@@ -1,10 +1,9 @@
+# 2.5gb used
 
-# BMTK Environment
+# BMTK Environment 
 conda create -y --name bmtk -c conda-forge -c kaeldai nest-simulator ipykernel matplotlib bmtk  # Install nestand BMTK
 conda install -y nb_conda_kernels
 
 ### References
 # Nest simulator- https://github.com/nest/nest-simulator
-
-
 #conda install -y -c conda-forge nest-simulator
