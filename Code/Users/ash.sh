dfe8e1c605c7a7110c340c8c4e876b54d3fb3e7e
Matthew Elliott <melliot1@ucsc.edu>
2022-02-11 13:36:39 -0800

    Update ash.sh

--- Code/Users/ash.sh
+++ Code/Users/ash.sh
@@ -4,9 +4,6 @@
 # Spike_Classiffication App
 pip install git+https://github.com/ahwillia/tensortools
 conda install -y py-xgboost
-pip install torch
-pip install colour
-pip install plotly
 apt-get install libgomp1
 
 
