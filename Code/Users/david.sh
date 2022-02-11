cfa9343f2b2d9a7518c46f439a8418a782806e91
Matthew Elliott <melliot1@ucsc.edu>
2022-02-11 13:38:36 -0800

    Update david.sh

--- Code/Users/david.sh
+++ Code/Users/david.sh
@@ -10,21 +10,21 @@
 #echo 'alias awsn="aws --endpoint https://s3.nautilus.optiputer.net"' >> ~/.bashrc
 
 # AWS Code
-apt-get install -y --no-install-recommends awscli # Install aws
-cd ~ && mkdir .aws && touch ~/.aws/credentials  # Setup for AWS credentials file
+#apt-get install -y --no-install-recommends awscli # Install aws
+#cd ~ && mkdir .aws && touch ~/.aws/credentials  # Setup for AWS credentials file
 # Set alias: https://stackoverflow.com/questions/36388465/how-to-set-bash-aliases-for-docker-containers-in-dockerfile
-echo -e '#!/bin/bash\naws --endpoint https://s3.nautilus.optiputer.net s3 "$@"' > /usr/bin/aws3 && chmod +x /usr/bin/aws3
-echo -e '#!/bin/bash\naws --endpoint https://s3.nautilus.optiputer.net "$@"' > /usr/bin/awsn && chmod +x /usr/bin/awsn
+#echo -e '#!/bin/bash\naws --endpoint https://s3.nautilus.optiputer.net s3 "$@"' > /usr/bin/aws3 && chmod +x /usr/bin/aws3
+#echo -e '#!/bin/bash\naws --endpoint https://s3.nautilus.optiputer.net "$@"' > /usr/bin/awsn && chmod +x /usr/bin/awsn
 
 
 # Packages for IoT Messaging in braingeneers
-pip3 install redis
-pip install s4cmd
-pip install -Iv awsiotsdk==1.5.7
-pip install -v awscrt==0.10.8
+#pip3 install redis
+#pip install s4cmd
+#pip install -Iv awsiotsdk==1.5.7
+#pip install -v awscrt==0.10.8
 
 # Install Kubernetes 
-apt -y install curl
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-rm kubectl
+#apt -y install curl
+#curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+#install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+#rm kubectl
