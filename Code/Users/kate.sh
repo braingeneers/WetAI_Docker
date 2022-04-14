12617466d766bac5469bf2785dee866f41266b48
Matthew Elliott <melliot1@ucsc.edu>
2022-04-13 18:27:08 -0700

    Update kate.sh

--- Code/Users/kate.sh
+++ Code/Users/kate.sh
@@ -1,11 +1,11 @@
 # 0.3gb used
 
 # Setup to run Kate's Code
-conda install -y -c plotly plotly-orca
-apt-get install -y xvfb libgtk2.0-0 libgconf-2-4
+#conda install -y -c plotly plotly-orca
+#apt-get install -y xvfb libgtk2.0-0 libgconf-2-4
 
-
-
+# 
+pip install -y pySerialTransfer
 
 
 
